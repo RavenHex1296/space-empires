2000eaeb92383dd066024c0abdb67bce955b90b4
Cayden Lau <suanoo5196@gmail.com>
2021-03-03 02:52:13 +0000

    finished game level 0.0

---
+++ tests/test_game_level_0_0.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('src')
+from game import *
+from player import *
+from random_player import *
+from custom_player import *
+
+players = [RandomPlayer(), CustomPlayer()]
+game = Game(players)
+
+assert game.game_state == {
+    'turn': 1,
+    'board_size': [7,7],
+    'players': {
+        1: {
+            'scout_coords': (4, 1),
+            'home_colony_coords': (4, 1)
+        },
+        2: {
+            'scout_coords': (4, 7),
+            'home_colony_coords': (4, 7)
+        }
+    },
+    'winner': None
+}
+
+game.complete_turn()
+print(game.game_state, "\n")
+
+game.run_to_completion()
+print(game.game_state)
