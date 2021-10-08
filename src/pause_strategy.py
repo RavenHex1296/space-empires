f7b9349f05cf8abb049dbae1b49c4253d5c8375d
Cayden Lau <69777457+RavenHex1296@users.noreply.github.com>
2021-10-08 01:49:41 +0000

    prior to standardization

--- src/pause_strategy.py
+++ src/pause_strategy.py
@@ -1,21 +1,53 @@
 import math
-
 import random
 
 class PauseStrategy():
     def __init__(self):
         self.counter = 0
 
+    def calculate_distance(self, initial_point, ending_point):
+        x = ending_point[0] - initial_point[0]
+        y = ending_point[1] - initial_point[1]
+        return (x ** 2 + y ** 2) ** 0.5
+
+    def min_distance_choice(self, choices, coordinate):
+        best_choice = choices[0]
+        min_distance = self.calculate_distance(best_choice, coordinate)
+
+        for choice in choices:
+            if self.calculate_distance(choice, coordinate) < min_distance:
+                best_choice = choice
+                min_distance = self.calculate_distance(choice, coordinate)
+
+        return best_choice
+
+    def min_distance_translation(self, choices, coordinate, desired_location):
+        best_choice = choices[0]
+        updated_coordinate = self.list_add(best_choice, coordinate)
+        dist = self.calculate_distance(updated_coordinate, desired_location)
+
+        for choice in choices:
+            new_coordinate = [choice[i]+coordinate[i] for i in range(len(choice))]
+
+            if self.calculate_distance(new_coordinate, desired_location) < dist:
+                best_choice = choice
+                updated_coordinate = new_coordinate
+                dist = self.calculate_distance(new_coordinate, desired_location)
+
+        return best_choice
+    
+    def list_add(self, x, y):
+        return [x[i]+y[i] for i in range(len(x))]
+
     def select_translation(self, ship_coords, choices, opp_home_cols):
-        if self.counter < 7:
+        if self.counter < 6:
             self.counter += 1
-            return (0, 0)
-        
-        else:
-            return (-1, 0)
+            return (0,0)
+        closest_col = self.min_distance_choice(opp_home_cols, ship_coords)
+        return self.min_distance_translation(choices, ship_coords, closest_col)
+    
+    def select_target(self, opponents):
+        if len(opponents) == 1:
+            return opponents[0]
 
-    def select_target(self, enemies):
-        if len(enemies)==1:
-            return enemies[0]
-
-        return enemies[random.randint(0, len(enemies)-1)]
+        return opponents[random.randint(0, len(opponents) - 1)]
--- src/test.py
+++ src/test.py
@@ -10,7 +10,7 @@ sys.path.append('src/old stuff')
 from custom_player import *
 from pause_strategy import *
 
-'''
+
 players = [Player(IllegalStrategy()), Player(IllegalStrategy())]
 game = Game(players)
 
@@ -26,9 +26,34 @@ game.complete_combat_phase()
 
 assert players[0].ships[0].coordinates == initial_coordinates
 print("PASSED")
-'''
+
+
 players = [Player(CustomStrategy()), Player(PauseStrategy())]
 game = Game(players)
-game.run_to_completion()
-print(game.winner)
 
+
+for _ in range(3):
+    game.complete_movement_phase()
+    game.complete_combat_phase()
+
+game.complete_movement_phase()
+
+
+combat_coordinates = game.combat_coordinates[0]
+
+all_objects = game.board[combat_coordinates[1]][combat_coordinates[0]]
+
+all_ships = [thing for thing in all_objects if isinstance(thing, Ship)]
+
+
+assert all_ships[0].player_number == 1
+print('PASSED')
+
+
+
+players = [Player(CustomStrategy()), Player(IllegalStrategy())]
+game = Game(players)
+game.run_to_completion()
+
+assert game.winner == 1
+print("PASSED")
